d0ee8b50b64a52f7dfe6046e100f45325c9a3511
Thomas Preston <thomasmarkpreston@gmail.com>
2013-06-20 13:58:21 +0100

    unblacklist used sed instead

--- bin/unblacklist-spi-bcm2708.sh
+++ bin/unblacklist-spi-bcm2708.sh
@@ -4,6 +4,7 @@
 
 blacklist_file="/etc/modprobe.d/raspi-blacklist.conf"
 blacklist_string="blacklist spi-bcm2708"
+unblacklist_string="#$blacklist_string # unblacklist for PiFace"
 tmp_blacklist_file="/tmp/new_backlist_file$(date +%Y%m%d%H%M)"
 
 # check if the script is being run as root
@@ -14,17 +15,11 @@ fi
 
 # if the file exists
 if [[ -a $blacklist_file ]]; then
-    blacklist_count=$(grep -c "^$blacklist_string" $blacklist_file)
-    
-    if [[ $blacklist_count -gt 0 ]]; then
-        # remove all mentions of spi module
-        grep -v "$blacklist_string" $blacklist_file > $tmp_blacklist_file
-        
-        # unblacklist
-        echo "#$blacklist_string # unblacklist for PiFace" >> $tmp_blacklist_file
-        
-        # overwrite
-        cp $blacklist_file $blacklist_file.old
-        mv $tmp_blacklist_file $blacklist_file
-    fi
+    # unblacklist
+    sed "s/^$blacklist_string$/$unblacklist_string/" $blacklist_file >> \
+        $tmp_blacklist_file
+    # backup
+    cp $blacklist_file $blacklist_file.old
+    # overwrite
+    mv $tmp_blacklist_file $blacklist_file
 fi
